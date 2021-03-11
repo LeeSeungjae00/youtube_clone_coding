86907a319c29ea6c42584380e3598db6c90e3607
LeeSeungjae00 <seungjae266789@gmail.com>
2021-03-11 23:01:35 +0900

    fetch 안되는 상황

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,41 @@
 import './app.css';
+import React, { useCallback, useEffect, useState } from 'react'
 import HeaderRapper from './components/header-rapper/headerRapper';
 import MainContent from './components/main-content/mainContent'
 
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
+
+// fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCg2DIwaT5af8y1NmV-PoXMjqk4d_3cZXg", requestOptions)
+//   .then(response => response.text())
+//   .then(result => console.log(result))
+//   .catch(error => console.log('error', error));
+
 function App() {
+  const [videoList, setVideoList] = useState([]);
+  const getPopular = useCallback(async () => {
+    try {
+      const response = await fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=24&key=AIzaSyCg2DIwaT5af8y1NmV-PoXMjqk4d_3cZXg", requestOptions)
+      const result = await response.text();
+      const {items} = await JSON.parse(result);
+
+      // .then(response => response.text())
+      // .then(result => JSON.parse(result));
+      // console.log(items);
+      return items
+    } catch (error) {
+      console.log(error)
+    }
+  },[])
+
+  useEffect(() => {
+    const items =getPopular();
+    console.log(items); 
+    return () => {
+    }
+  }, [getPopular])
   return (
     <>
       <HeaderRapper></HeaderRapper>
