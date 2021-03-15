b4c189101f71e049ac84912790ab65b9aacad134
LeeSeungjae <seungjae2668@gsinstrument.com>
2021-03-15 17:04:00 +0900

    videoCard css 추가 전체적인 모양과 색상을 맞춤

--- src/components/videoCard/videoCard.jsx
+++ src/components/videoCard/videoCard.jsx
@@ -3,9 +3,8 @@ import './videoCard.css';
 
 export default function videoCard({info}) {
     const {thumbnails, title, channelTitle} = info;
-    console.log(info)
     return (
-        <div className = "videoCard">
+        <div onClick = {()=> {console.log(info)}} className = "videoCard">
             <img style = {{width : 304, height : 171}} src= {thumbnails.medium.url}alt="thum"/>
             <p style = {{width : 304}}>{title}</p>
             <p style = {{widtj : 304}}>{channelTitle}</p>
