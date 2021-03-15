5616e32f399ef542e0c578dadd34d84878e33c5b
LeeSeungjae <seungjae2668@gsinstrument.com>
2021-03-15 17:12:48 +0900

    타이틀과 채널명도 css 수정

--- src/components/videoCard/videoCard.jsx
+++ src/components/videoCard/videoCard.jsx
@@ -5,9 +5,9 @@ export default function videoCard({info}) {
     const {thumbnails, title, channelTitle} = info;
     return (
         <div onClick = {()=> {console.log(info)}} className = "videoCard">
-            <img style = {{width : 304, height : 171}} src= {thumbnails.medium.url}alt="thum"/>
-            <p style = {{width : 304}}>{title}</p>
-            <p style = {{widtj : 304}}>{channelTitle}</p>
+            <img className = "thumbnails" style = {{width : 304, height : 171}} src= {thumbnails.medium.url}alt="thum"/>
+            <p className = "title">{title}</p>
+            <p className = "channelTitle">{channelTitle}</p>
         </div>
     )
 }
